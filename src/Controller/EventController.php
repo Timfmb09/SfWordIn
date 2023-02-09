ebfc39130b8688d9e450f86a64b1ab15917a3911
Timfmb09 <tim.fmb@gmail.com>
2023-02-09 22:05:59 +0100

    Debug css Symfony6

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -7,6 +7,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class EventController extends AbstractController
 {
@@ -20,12 +21,33 @@ class EventController extends AbstractController
         ]);
     }
 
+    #[Route('/event/add', name: 'add_event')]
+    public function add(ManagerRegistry $doctrine, Event $event = null, Request $request): Response
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $event = $doctrine->getManager();
+            $event->persist($event);
+            $event->flush();
+
+            return $this->redirectToRoute('app_event_index');
+        }
+
+        //Vue pour afficher le formulaire d'ajout d'un évènement
+        return $this->render('event/add.html.twig', [
+        ]);
+
+    }
+
     #[Route('/event/{id}', name: 'show_event')]
     public function show(Event $event): Response
     {
-    
         return $this->render('event/show.html.twig', [
             'event' => $event,
         ]);
     }
+
+ 
 }
